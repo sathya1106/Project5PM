da3e5050eb155acaa94909ca124c10f0078d5df0
sathya1106 <sathyasri1102@gmail.com>
2022-02-01 10:15:29 +0530

    Sri's New Code

--- src/test/java/org/test/Sample.java
+++ src/test/java/org/test/Sample.java
@@ -5,6 +5,8 @@ public class Sample {
        System.out.println("Test1");
        System.out.println("Test2");
        System.out.println("Test3");
+       System.out.println("Test6");
+       System.out.println("Test7");
 
 	}
 }
